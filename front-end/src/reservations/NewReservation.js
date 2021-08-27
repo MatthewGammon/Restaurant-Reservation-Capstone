4b097d1cb4fa9f748939a8e4067203dd92a847cf
Matthew Gammon <mgammon91@gmail.com>
2021-08-27 01:44:50 -0400

    added a 'catch' to the submit handler which makes a request to the /reservations endpoint. added the ErrorAlert component which renders only if an error is caught

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { createReservation } from '../utils/api';
+import ErrorAlert from '../layout/ErrorAlert';
 
 export default function NewReservation() {
+  const [reservationsError, setReservationsError] = useState(null);
   const [reservation, setReservation] = useState({
     first_name: '',
     last_name: '',
@@ -16,9 +18,11 @@ export default function NewReservation() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    createReservation(reservation).then(() =>
-      history.push(`/dashboard/?date=${reservation.reservation_date}`)
-    );
+    createReservation(reservation)
+      .then(() =>
+        history.push(`/dashboard/?date=${reservation.reservation_date}`)
+      )
+      .catch(setReservationsError);
   }
 
   function handleChange({ target: { name, value } }) {
@@ -30,6 +34,7 @@ export default function NewReservation() {
 
   return (
     <form className="reservation-form" onSubmit={handleSubmit}>
+      <ErrorAlert error={reservationsError} />
       <label>
         First Name:
         <input
@@ -46,7 +51,7 @@ export default function NewReservation() {
         <input
           name="last_name"
           type="text"
-          placeholder="Bobbinson"
+          placeholder="Boberts"
           required
           value={reservation.last_name}
           onChange={handleChange}
@@ -59,7 +64,7 @@ export default function NewReservation() {
           name="mobile_number"
           type="tel"
           placeholder="555-666-6969"
-          // pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
           required
           value={reservation.mobile_number}
           onChange={handleChange}
