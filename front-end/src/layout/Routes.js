47781e96647564a2390525642392e6cfbb089140
Matthew Gammon <mgammon91@gmail.com>
2021-08-30 11:13:55 -0400

    added a route to /tables/new which renders the NewTable component

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -5,6 +5,7 @@ import Dashboard from '../dashboard/Dashboard';
 import NotFound from './NotFound';
 import { today } from '../utils/date-time';
 import NewReservation from '../reservations/NewReservation';
+import NewTable from '../tables/NewTable';
 import useQuery from '../utils/useQuery';
 
 /**
@@ -26,6 +27,9 @@ function Routes() {
       </Route>
       <Route exact={true} path="/reservations/new">
         <NewReservation />
+      </Route>
+      <Route exact={true} path="/tables/new">
+        <NewTable />
       </Route>
       <Route path="/dashboard">
         <Dashboard date={query.get('date') || today()} />
