3df6c1169be0a77beb71937c2b8718b2e79ceeb9
Matthew Gammon <mgammon91@gmail.com>
2021-09-01 16:44:21 -0400

    added read and updateTable functions. modified the create function to only return the first element

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,7 +1,28 @@
 const knex = require('../db/connection');
 
 function create(table) {
-  return knex('tables').insert(table).returning('*');
+  return knex('tables')
+    .insert(table)
+    .returning('*')
+    .then((createdTable) => createdTable[0]);
+}
+
+function read(reservationId) {
+  return knex('tables').select('*').where({ reservation_id: reservationId });
+}
+
+function updateTable(reservationId, tableId) {
+  return knex('tables')
+    .join(
+      'reservations',
+      'tables.reservation_id',
+      'reservations.reservation_id'
+    )
+    .select('*')
+    .where({ table_id: tableId })
+    .update('reservation_id', reservationId)
+    .update('occupied', true)
+    .then(() => read(reservationId));
 }
 
 function list() {
@@ -10,5 +31,7 @@ function list() {
 
 module.exports = {
   create,
+  read,
+  updateTable,
   list,
 };
