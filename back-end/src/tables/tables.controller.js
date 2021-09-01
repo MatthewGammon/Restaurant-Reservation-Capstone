c4dc2a51a7ac341f3365ccf46eea842143d7fc33
Matthew Gammon <mgammon91@gmail.com>
2021-09-01 16:41:14 -0400

    created validation middleware to check table capacity, table name, and that all required properties are present. added list, create, and update functions

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,5 +1,49 @@
 const service = require('./tables.service');
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+const { table } = require('../db/connection');
+
+const validProperties = ['table_name', 'capacity'];
+
+function hasProperties(req, res, next) {
+  const { data = {} } = req.body;
+  validProperties.forEach((property) => {
+    if (!data[property]) {
+      next({
+        status: 400,
+        message: `A '${property}' property is required.`,
+      });
+    }
+  });
+  next();
+}
+
+function hasValidName(req, res, next) {
+  const { table_name } = req.body.data;
+  if (!table_name || table_name === '' || table_name.length < 2) {
+    next({
+      status: 400,
+      message: `The table must have a table_name property that is at least two characters long.`,
+    });
+  }
+  next();
+}
+
+function hasValidCapacity(req, res, next) {
+  const { capacity } = req.body.data;
+  if (!capacity || capacity < 1 || typeof capacity !== 'number') {
+    next({
+      status: 400,
+      message: `The table must have a capacity of at least 1.`,
+    });
+  }
+  next();
+}
+
+async function updateTable(req, res, next) {
+  const reservation_id = req.body.data.reservation_id;
+  const table_id = req.body.data.table_id;
+  res.json({ data: await service.updateTable(reservation_id, table_id) });
+}
 
 async function create(req, res, next) {
   const newTable = await service.create(req.body.data);
@@ -11,6 +55,13 @@ async function list(req, res, next) {
 }
 
 module.exports = {
-  create: asyncErrorBoundary(create),
+  create: [
+    // asyncErrorBoundary(hasValidProperties),
+    asyncErrorBoundary(hasProperties),
+    asyncErrorBoundary(hasValidName),
+    asyncErrorBoundary(hasValidCapacity),
+    asyncErrorBoundary(create),
+  ],
+  update: asyncErrorBoundary(updateTable),
   list: asyncErrorBoundary(list),
 };
