384067f6b09b048d30ddeddf749b663ce5f8f5ed
Matthew Gammon <mgammon91@gmail.com>
2021-09-10 04:57:17 -0400

    changed name of updateTable func to 'update'. update func now updates the reservation status to 'seated' as well as assigning a reservation_id to table.reservation_id. updated clearTable func to update res status to 'finished' as well as updating table.reservation_id to null

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -11,27 +11,38 @@ function readTable(tableId) {
   return knex('tables').select('*').where({ table_id: tableId }).first();
 }
 
-function updateTable(reservationId, tableId) {
-  return knex('tables')
-    .where({ table_id: tableId })
-    .update('reservation_id', reservationId)
-    .then(() => readTable(tableId));
+function update(reservationId, tableId) {
+  return knex('reservations')
+    .where({ reservation_id: reservationId })
+    .update({ status: 'seated' })
+    .then(() => {
+      return knex('tables')
+        .where({ table_id: tableId })
+        .update({ reservation_id: reservationId })
+        .returning('*');
+    });
 }
 
 function list() {
   return knex('tables').returning('*').orderBy('table_name');
 }
 
-function clearTable(tableId) {
-  return knex('tables')
-    .where({ table_id: tableId })
-    .update({ reservation_id: null })
-    .then(() => readTable(tableId));
+function clearTable(tableId, reservationId) {
+  return knex('reservations')
+    .where({ reservation_id: reservationId })
+    .update({ status: 'finished' })
+    .returning('*')
+    .then(() => {
+      return knex('tables')
+        .where({ table_id: tableId })
+        .update({ reservation_id: null })
+        .then(() => readTable(tableId));
+    });
 }
 
 module.exports = {
   create,
-  updateTable,
+  update,
   list,
   readTable,
   clearTable,
