030f4fb75225d1ea75db1d899af8b40a28c3c803
Matthew Gammon <mgammon91@gmail.com>
2021-09-14 08:48:32 -0400

    updated useEffect to include a loadReservation function that makes a GET request if a reservationId is present in params and then sets the state of 'reservation' to the returned data. updated onSubmit to make a PUT request if a resId is present from params or otherwise makes a POST request to create a new reservation

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useHistory } from 'react-router-dom';
-import { createReservation } from '../utils/api';
+import { useHistory, useParams } from 'react-router-dom';
+import {
+  createReservation,
+  updateReservation,
+  readReservation,
+} from '../utils/api';
 import ErrorAlert from '../layout/ErrorAlert';
 
 export default function NewReservation() {
@@ -16,19 +20,45 @@ export default function NewReservation() {
   });
 
   const history = useHistory();
+  const params = useParams();
+  const resId = params.reservationId;
+
   const inputRef = useRef();
 
   useEffect(() => {
     inputRef.current.focus();
-  }, []);
+    async function loadReservation() {
+      const abortController = new AbortController();
+      setReservationsError(null);
+      try {
+        if (resId) {
+          const originalReservation = await readReservation(
+            resId,
+            abortController.signal
+          );
+          setReservation(originalReservation);
+        }
+      } catch (error) {
+        setReservationsError(error);
+      }
+      return () => abortController.abort();
+    }
+    loadReservation();
+  }, [resId]);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    createReservation(reservation)
-      .then(() =>
-        history.push(`/dashboard/?date=${reservation.reservation_date}`)
-      )
-      .catch(setReservationsError);
+    try {
+      if (resId) {
+        await updateReservation(reservation);
+        history.push(`/dashboard?date=${reservation.reservation_date}`);
+      } else {
+        await createReservation(reservation);
+        history.push(`/dashboard?date=${reservation.reservation_date}`);
+      }
+    } catch (error) {
+      setReservationsError(error);
+    }
   }
 
   function handleChange({ target: { name, value } }) {
