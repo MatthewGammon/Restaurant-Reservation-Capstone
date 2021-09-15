acdb6d853b8f2d828c108fe6f39fbd4eaef71966
Matthew Gammon <mgammon91@gmail.com>
2021-09-15 12:02:20 -0400

    removed two minor pieces of bootstrap styling

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 /**
  * Defines the menu for this application.
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
+    <nav className="navbar  align-items-start p-0">
       <div className="container-fluid d-flex flex-column p-0">
         <Link
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
@@ -21,7 +21,7 @@ function Menu() {
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
+        <ul className="nav navbar-nav " id="accordionSidebar">
           <li className="nav-item">
             <Link className="nav-link" to="/dashboard">
               <span className="oi oi-dashboard" />
