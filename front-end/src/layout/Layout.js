f7e4b983e4f347d462d2df6aed42a69c62ff89d8
Matthew Gammon <mgammon91@gmail.com>
2022-02-04 13:22:36 -0500

    removed unnecessary divs and bootstrap styling

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -1,8 +1,6 @@
-import React from "react";
-import Menu from "./Menu";
-import Routes from "./Routes";
-
-import "./Layout.css";
+import React from 'react';
+import Navbar from './Navbar';
+import Routes from './Routes';
 
 /**
  * Defines the main layout of the application.
@@ -13,16 +11,11 @@ import "./Layout.css";
  */
 function Layout() {
   return (
-    <div className="container-fluid">
-      <div className="row h-100">
-        <div className="col-md-2 side-bar">
-          <Menu />
-        </div>
-        <div className="col">
-          <Routes />
-        </div>
-      </div>
-    </div>
+    <>
+      {' '}
+      <Navbar />
+      <Routes />
+    </>
   );
 }
 
