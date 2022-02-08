5e7f8a83c7037262ed564412499a1475313084b6
Matthew Gammon <mgammon91@gmail.com>
2022-02-08 03:38:32 -0500

    removed margin from error alert to allow for it to align with the content it is displayed alongside

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 /**
  * Defines the alert message to render if the specified error is truthy.
@@ -10,9 +10,7 @@ import React from "react";
 
 function ErrorAlert({ error }) {
   return (
-    error && (
-      <div className="alert alert-danger m-2">Error: {error.message}</div>
-    )
+    error && <div className="alert alert-danger">Error: {error.message}</div>
   );
 }
 
