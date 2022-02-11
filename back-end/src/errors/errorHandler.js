cc68e62f07d6b6580c23de96fa5ff7c9259afb5b
Matthew Gammon <mgammon91@gmail.com>
2022-02-11 18:23:15 -0500

    removed comments

--- back-end/src/errors/errorHandler.js
+++ back-end/src/errors/errorHandler.js
@@ -1,8 +1,5 @@
-/**
- * Express API error handler.
- */
 function errorHandler(error, request, response, next) {
-  const { status = 500, message = "Something went wrong!" } = error;
+  const { status = 500, message = 'Something went wrong!' } = error;
   response.status(status).json({ error: message });
 }
 
