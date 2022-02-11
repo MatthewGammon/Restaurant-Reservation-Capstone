d67bd313b29876406928dd3a510c289f0a486bed
Matthew Gammon <mgammon91@gmail.com>
2022-02-11 17:47:42 -0500

    removed H1 and text, added 404 image. justified content

--- front-end/src/layout/NotFound.js
+++ front-end/src/layout/NotFound.js
@@ -1,16 +1,10 @@
-import React from "react";
+import React from 'react';
+import burger404 from '../assets/images/burger-404.jpg';
 
-/**
- * Defines the "Not Found" page that is displayed for any unmatched route.
- *
- * You will not need to make changes to this file.
- *
- * @returns {JSX.Element}
- */
 function NotFound() {
   return (
-    <div className="NotFound">
-      <h1>Not Found</h1>
+    <div className="notFound d-flex justify-content-center mt-5">
+      <img src={burger404} alt="hamburger 404" />
     </div>
   );
 }
